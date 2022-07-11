723d2831472cd09aaccc098b6675399ff65b8c51
zeeshan <skzeeshan3650@gmail.com>
2022-07-12 04:39:07 +0530

    Commit 2

--- reiserx/urls.py
+++ reiserx/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('policy/', views.policy, name="policy"),
-    path('terms/', views.policy, name="terms of use"),
+    path('terms of use/', views.terms, name="terms of use"),
+    path('contact', views.contact, name="contact"),
 ]
--- reiserx/views.py
+++ reiserx/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import pyrebase
 
 
 def home(request):
@@ -12,3 +12,27 @@ def policy(request):
 
 def terms(request):
     return render(request, "termsofuse.html")
+
+
+def contact(request):
+    config = {
+        "apiKey": "AIzaSyBxfeEYK42Z9_5J9QzHXOX1yxHUgVR-i8U",
+        "authDomain": "testtrace-941f7.firebaseapp.com",
+        "databaseURL": "https://testtrace-941f7-default-rtdb.firebaseio.com",
+        "projectId": "testtrace-941f7",
+        "storageBucket": "testtrace-941f7.appspot.com",
+        "messagingSenderId": "145231816902",
+        "appId": "1:145231816902:web:df232c954486dd83254fc7",
+        "measurementId": "G-FCM9GH77KN"
+    }
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+
+    val1 = request.POST['fullname']
+    val2 = request.POST['email']
+    val3 = request.POST['message']
+
+    data = {"fullname": val1, "email": val2, "message": val3}
+    db.child("Administration").child("Web").child("contact").push(data)
+
+    return render(request, "index.html")
