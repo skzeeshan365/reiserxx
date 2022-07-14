6c011320b626f62a929b2b82441b6b2d1edbe401
zeeshan <skzeeshan3650@gmail.com>
2022-07-15 00:11:35 +0530

    Commit 7

--- reiserx/urls.py
+++ reiserx/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('policy/', views.policy, name="policy"),
+    path('privacypolicy/', views.policy, name="policy"),
     path('terms of use/', views.terms, name="terms of use"),
     path('contact', views.contact, name="contact"),
     path('setup', views.setupguide, name="setup"),
