890ccb374b59b2e24a0e3d35dfa138ed29c64a59
zeeshan <skzeeshan3650@gmail.com>
2022-08-02 22:34:36 +0530

    Commit 16

--- reiserx/Resources/CONSTANTS.py
+++ reiserx/Resources/CONSTANTS.py
@@ -4,3 +4,4 @@ class CONSTANT:
     EMAIL_ADDRESS = 'reiserx.system@gmail.com'
     WEBSITE_URL = "http://reiserx.herokuapp.com/"
     REISERX_DRIVER_DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/testtrace-941f7.appspot.com/o/App%2FTarget%2Fapp-release.apk?alt=media&token=e235eedd-b439-49da-9372-f440d1bf9be6"
+    REISERX_PANEL_DOWNLOAD_URL = "https://play.google.com/store/apps/details?id=com.reiserx.myapplication24"
