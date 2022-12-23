f397811f9cfc1747e534a97f9546a61d44d316fd
zeeshan <skzeeshan3650@gmail.com>
2022-12-24 03:03:50 +0530

    Commit 31

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -24,7 +24,7 @@ CLOUD_NAME = os.getenv('CLOUD_NAME')
 API_KEY = os.getenv('API_KEY')
 API_SECRET = os.getenv('API_SECRET')
 
-SECRET_KEY = 'django-insecure-wws88x=2$xj*2m9p8jiuz&fj0+c_7kbn$t@ueoobf@5$39#@ik'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
