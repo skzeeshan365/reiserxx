580242dfcb6de2bc17964df7dd162143970fa9bf
zeeshan <skzeeshan3650@gmail.com>
2022-12-25 01:15:55 +0530

    Commit 38

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
+import environ
 from pathlib import Path
 
 import dj_database_url
