a8c6ab10df8f8fa4e6833434fa41b3afaa59b963
zeeshan <skzeeshan3650@gmail.com>
2023-04-24 21:12:29 +0530

    fix 2

--- testApp/views.py
+++ testApp/views.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 from django.shortcuts import render
 
-def predict(request):
-    if request.method == 'POST':
 
-    else:
-        return render(request, 'form.html')
+def predict(request):
+    return render(request, 'form.html')
