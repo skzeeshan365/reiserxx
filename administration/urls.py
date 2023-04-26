7846bc09c078176f09a0b74dd872280aa8092bf6
zeeshan <skzeeshan3650@gmail.com>
2023-04-26 17:02:40 +0530

    fix 6

--- administration/urls.py
+++ administration/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', login_required(user_passes_test(lambda u: u.is_superuser)(views.post_create_view)), name='post_new'),
     path('login', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('category/', login_required(user_passes_test(lambda u: u.is_superuser)(views.category)), name='category'),
+    path('category/', login_required(user_passes_test(lambda u: u.is_superuser)(views.category)), name='categories'),
     path('post/', login_required(user_passes_test(lambda u: u.is_superuser)(views.post_edit)), name='post_edit'),
 ]
