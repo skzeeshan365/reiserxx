0fbe24730b703ba9310c7d979018459e24f51d2e
zeeshan <skzeeshan3650@gmail.com>
2023-05-12 17:16:25 +0530

    optimized images

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CLOUDINARY_STORAGE = {
