bb8a191c701bf17c377b5213df4906e0a8290d45
zeeshan <skzeeshan3650@gmail.com>
2023-05-14 05:14:49 +0530

    added translation

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '54.238.218.120', 'localhost', 'reiserx.com']
+ALLOWED_HOSTS = ['127.0.0.1', '54.238.76.143', 'localhost', 'reiserx.com']
 
 # Application definition
 
