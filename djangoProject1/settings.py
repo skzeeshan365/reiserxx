45b95ab45835d4da263f744156f3745f69228d71
zeeshan <skzeeshan3650@gmail.com>
2023-05-14 08:36:52 +0530

    added translation

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -14,7 +14,7 @@ API_SECRET = os.getenv('API_SECRET')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '54.238.76.143', 'localhost', 'reiserx.com']
 
@@ -155,6 +155,25 @@ VERIFALIA_PASSWORD = os.getenv('VERIFALIA_PASSWORD')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 WHITENOISE_USE_FINDERS = True
 
