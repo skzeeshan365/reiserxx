550f6f138b1a828ef829a7ccf24e41c99efa16a8
zeeshan <skzeeshan3650@gmail.com>
2023-05-29 21:45:55 +0530

    added favicons

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '54.199.167.173', 'localhost', 'reiserx.com']
+ALLOWED_HOSTS = ['127.0.0.1', '54.199.167.173', '13.233.151.164', 'localhost', 'reiserx.com']
 
 # Application definition
 
