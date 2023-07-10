634fcc75c6fd0332e6b5c12e06154b0416e673b2
zeeshan <skzeeshan3650@gmail.com>
2023-07-10 13:31:32 +0530

    updated home

--- testApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.predict, name='apsa'),
-    path('predict/', views.predict, name='predict'),
-]
--- testApp/views.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-from django.shortcuts import render
-
-
-def predict(request):
-    return render(request, 'form.html')
