05f6e476c4573e44eae1eaa0ed2968c26c7e0a60
zeeshan <skzeeshan3650@gmail.com>
2023-08-20 22:40:13 +0530

    updated home

--- main/feeds.py
+++ main/feeds.py
@@ -1,11 +1,15 @@
 from django.contrib.syndication.views import Feed
 from django.urls import reverse
+from django.utils import feedgenerator
+
 from .models import Post  # Import your Post model
+
 
 class BlogFeed(Feed):
     title = "ReiserX"  # Your feed's title
     link = "https://www.reiserx.com/blog/"  # URL for the feed
     description = "Latest posts from ReiserX"  # Feed's description
+    template_name = "main/partials/postlist.html"  # Use your custom template
 
     def items(self):
         return Post.objects.filter(draft=False)  # Filter published posts
@@ -18,6 +22,10 @@ class BlogFeed(Feed):
 
     def item_link(self, item):
         return reverse('open', args=[item.author.username, item.slug])  # Link to each post
+
+    def item_enclosures(self, item):
+        return [feedgenerator.Enclosure(item.image.url, str(item.image.size),
+                                        'image/{}'.format(item.image.name.split('.')[-1]))]
 
     def item_pubdate(self, item):
         return item.timestamp  # Publish date of each post
