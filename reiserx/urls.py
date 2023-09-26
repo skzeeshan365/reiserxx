e25b4ed9982df39a5e8d62073d62604a29b0041f
zeeshan <skzeeshan3650@gmail.com>
2023-09-26 18:27:42 +0530

    updated summary

--- reiserx/urls.py
+++ reiserx/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='reiserx_home'),
-    path('privacy-policy/', views.policy, name="policy"),
+    path('privacy-policy/', views.policy, name="panel_policy"),
     path('reiserx-privacy-policy/', views.reiserxpolicy, name="ReiserX privacy policy"),
     path('terms of use/', views.terms, name="terms of use"),
     path('contact', views.contact, name="reiserx_contact"),
