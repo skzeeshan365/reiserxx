d86b00a4f056128187a5a869c31121bd5cf457e6
zeeshan <skzeeshan3650@gmail.com>
2023-11-27 17:57:57 +0530

    updated compress_images

--- message/urls.py
+++ message/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='message_home'),
+    path('mail/', views.mail, name='message_mail'),
 ]
--- message/views.py
+++ message/views.py
@@ -1,11 +1,31 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+
+from main import utils
+
 
 # Create your views here.
 
 
 def home(request):
-    raise Http404
+    return render(request, 'secondary/Test/index.html')
+
+
+def mail(request):
+    if request.method == 'POST':
+        subject = "A New Message Is Received"
+
+        message = "Hurrray, She said yes."
+
+        to_email = 'skzeeshan3650@gmail.com'
+
+        try:
+            utils.send_email(subject=subject, message=message, to_email=to_email)
+            return HttpResponse("Email sent successfully!")
+        except Exception as e:
+            pass
+    else:
+        raise Http404
 
 
 def no(request):
