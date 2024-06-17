a176d51b73081f652da94ed72a3d96c2ad26aeeb
zeeshan <skzeeshan3650@gmail.com>
2024-06-17 23:28:40 +0530

    add ai multimodel

--- ai/views.py
+++ ai/views.py
@@ -33,31 +33,32 @@ def multimodel(request):
     if request.method == 'POST':
         auth_header = request.headers.get('Authorization')
         if auth_header != os.getenv('PYTHON_KEY'):
-            return JsonResponse({'error': 'Unauthorized'}, status=401)
+            return HttpResponse('Unauthorized', status=401)
 
         img = request.FILES.get('image')
         text = request.POST.get('text')
 
         if not img or not text:
-            return JsonResponse({'error': 'File and text data are required'}, status=400)
+            return HttpResponse('File and text data are required', status=400)
 
         try:
             img = Image.open(img)
             # Optionally, you can resize or preprocess the image here if required
         except Exception as e:
-            return JsonResponse({'error': 'Failed to process image: ' + str(e)}, status=400)
+            return HttpResponse('Failed to process image: ' + str(e), status=400)
 
-        # genai.configure(api_key=settings.GEMINI_API)
-        # model = genai.GenerativeModel('gemini-1.5-flash')
-        # response = model.generate_content([
-        #     "describe this image.",
-        #     img], stream=True)
-        # response.resolve()
+        try:
+            # genai.configure(api_key=settings.GEMINI_API)
+            # model = genai.GenerativeModel('gemini-1.5-flash')
+            # response = model.generate_content([
+            #     text,
+            #     img], stream=True)
+            # response.resolve()
 
-        response = json.dumps({"text": "Successful"})
-
-        # Return JSON response with generated content
-        return JsonResponse(response)
+            response = 'response.text'
+            return JsonResponse({"response": response})
+        except Exception as e:
+            return HttpResponse('Failed to generate response, please try again later', status=500)
 
         # Handle other HTTP methods (GET, PUT, DELETE, etc.) if needed
-    return JsonResponse({'error': 'Method not allowed'}, status=405)
+    return HttpResponse('Method not allowed', status=405)
